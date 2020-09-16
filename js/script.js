fe7640dee3f0500b2aff1cee5a61f64f6adc78e7
raj2411 <52874785+raj2411@users.noreply.github.com>
2020-09-16 12:48:01 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+	$("#y1").click(function () {
+		$("#x1").hide();
+	});
+});
+$(document).ready(function () {
+	$("#y2").click(function () {
+		$("#x1").show();
+	});
+});
+$(document).ready(function () {
+	$("#y3").click(function () {
+		$("#x2").hide();
+	});
+});
+$(document).ready(function () {
+	$("#y4").click(function () {
+		$("#x2").show();
+	});
+});
+$(document).ready(function () {
+	$("#y5").click(function () {
+		$("#x3").hide();
+	});
+});
+$(document).ready(function () {
+	$("#y6").click(function () {
+		$("#x3").show();
+	});
+});
+$(document).ready(function () {
+	$("#y7").click(function () {
+		$("#x4").hide();
+	});
+});
+$(document).ready(function () {
+	$("#y8").click(function () {
+		$("#x4").show();
+	});
+});
